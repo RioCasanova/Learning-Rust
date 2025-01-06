63b1dcda62aec8ad524d069b439b67cc614e0af7
Rio Casanova <90357505+RioCasanova@users.noreply.github.com>
2025-01-05 22:53:32 -0700

    minor changes

--- basic_web_scraper/src/main.rs
+++ basic_web_scraper/src/main.rs
@@ -8,10 +8,6 @@ use reqwest::Client;
 
     Purpose: 
 
-        Build a better version of 'basic_web_scraper' that uses less binary code
-        and is more intuitive. Meaning that it should be easier to use, and 
-        easier to know how to code it from memory.
-
         The overall purpose is to build Rust experience and skills as
         well as familiarity with async programming and other crates.
 */
--- basic_web_scraper_v2/src/main.rs
+++ basic_web_scraper_v2/src/main.rs
@@ -1,5 +1,19 @@
 use reqwest::Client;
 
+/*
+    Author: Rio Casanova
+    Created: January 5, 2025
+    Version: 2
+
+    Purpose: 
+
+        Build a better version of 'basic_web_scraper' that uses less binary code
+        and is more intuitive. Meaning that it should be easier to use, and 
+        easier to know how to code it from memory.
+
+        The overall purpose is to build Rust experience and skills as
+        well as familiarity with async programming and other crates.
+*/
 
 #[tokio::main]
 async fn main() {
